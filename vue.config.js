ae4a08925058d46343ce1f464bfeefba20c56c8d
jardenliu <15915839707@163.com>
2019-03-04 16:59:20 +0800

    updated: vue config

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,23 @@
-const { resolve } = require('path')
+const {
+  resolve
+} = require('path')
 module.exports = {
   pages: {
     index: {
       entry: 'src/renderer/main.ts'
     }
   },
-  // configureWebpack: config => {
-  //   console.log(config.resolve)
-  // },
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': resolve(__dirname, 'src/renderer')
-      }
-    }
+  configureWebpack: config => {
+    config.resolve.extensions = ['.ts', '.tsx', '.vue', '.mjs', '.js', '.jsx', '.json', '.wasm']
+    config.resolve.modules.push(resolve(__dirname, 'src'))
+    config.resolve.alias['@'] = resolve(__dirname, 'src/renderer')
   },
   pluginOptions: {
     // vue-cli-plugin-electron-builder Doc, check ou: https://nklayman.github.io/vue-cli-plugin-electron-builder/
     electronBuilder: {
       // To see available options, check out https://www.electron.build/configuration/configuration
       builderOptions: {
-        appId: 'com.example.app'
+        appId: 'vip.jarden.mool'
       },
       mainProcessFile: 'src/main/background.ts'
     }
